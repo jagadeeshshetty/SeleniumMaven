0857fdc52aa83f305fedee12611a602820cf974d
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2021-03-21 12:32:27 +0530

    update

--- src/test/java/helper/Base.java
+++ src/test/java/helper/Base.java
@@ -1,8 +1,11 @@
 package test.java.helper;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.apache.log4j.Logger;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import test.java.tests.TestLoginGA;
 
 import java.util.concurrent.TimeUnit;
 
@@ -10,11 +13,17 @@ public class Base {
 
     private WebDriver driver;
     public static ThreadLocal<WebDriver> tDriver = new ThreadLocal<WebDriver>();
+    private static final Logger logger = helper.LoggerHelper.getLogger(TestLoginGA.class);
 
     public WebDriver initDriver() {
+        System.out.println(System.getProperty("os.name"));
         WebDriverManager.chromedriver().setup();
-        driver = new ChromeDriver();
-        System.out.println(driver.toString());
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--no-sandbox");
+        options.addArguments("--disable-dev-shm-usage");
+        options.addArguments("--headless");
+        driver = new ChromeDriver(options);
+        if (driver != null) logger.info(driver.toString());
         driver.manage().timeouts().pageLoadTimeout(48, TimeUnit.SECONDS);
         tDriver.set(driver);
         return getDriver();
