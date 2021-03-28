a61edb78b210c7b2cc55f0d3cfa3500558f4bfcb
JC <jagadeesh.cheluvaraj@gmail.com>
2021-03-28 13:44:51 +0530

    testing retry support

--- src/test/java/helper/Retry.java
+++ src/test/java/helper/Retry.java
@@ -6,7 +6,7 @@ import org.testng.ITestResult;
 public class Retry implements IRetryAnalyzer {
 
     private int retryCount = 0;
-    private static final int maxRetryCount = 3;
+    private static final int maxRetryCount = 2;
 
     @Override
     public boolean retry(ITestResult result) {
