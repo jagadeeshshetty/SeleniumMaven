e327c7c4db55a52e4ab060dfebbe6383fa536a68
JagadeeshC <jagadeesh.cheluvaraj@gmail.com>
2021-04-08 14:54:15 +0530

    update

--- src/test/java/helper/Base.java
+++ src/test/java/helper/Base.java
@@ -15,6 +15,8 @@ import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeMethod;
 
 import java.lang.invoke.MethodHandles;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.concurrent.TimeUnit;
 
 import static io.qameta.allure.Allure.step;
@@ -23,27 +25,25 @@ import static test.java.helper.GlobalConfig.browser;
 public class Base {
 
     public static final Logger logger = Logger.getLogger(MethodHandles.lookup().lookupClass().getSimpleName());
-    ChromeOptions chromeOptions;
     protected static ThreadLocal<WebDriver> driver = new ThreadLocal<>();
+
+    ChromeOptions chromeOptions;
+
+    public WebDriver getDriver() {
+        return driver.get();
+    }
 
     @BeforeMethod
     public void setup(ITestContext context) {
-        System.out.println(">>>>>>>>> before");
-//        WebDriverManager.chromedriver().setup();
-//        driver.set(new ChromeDriver());
+        step("Start time: " + getDateTime("yyyy/MM/dd HH:mm:ss"));
         step("Running Test on '" + System.getProperty("os.name") + "' OS.");
-        logger.info("Running Test on '" + System.getProperty("os.name") + "' OS.");
         step("Browser: " + browser.toUpperCase());
-        logger.info("Browser: " + browser.toUpperCase());
-
         switch (browser) {
             case "firefox":
                 WebDriverManager.firefoxdriver().setup();
                 driver.set(new FirefoxDriver());
                 break;
             case "chromeHeadless":
-                step("Browser: Chrome Headless");
-                logger.info("Browser: Chrome Headless");
                 WebDriverManager.chromedriver().setup();
                 chromeOptions = new ChromeOptions();
                 chromeOptions.addArguments("--no-sandbox");
@@ -78,94 +78,25 @@ public class Base {
                 logger.info("No proper browser specified.");
         }
         if (driver != null) {
-            step("Driver instance: " + driver.toString());
-            logger.info("Driver instance: " + driver.toString());
-            getDriver().manage().timeouts().pageLoadTimeout(48, TimeUnit.SECONDS);
+            step("Driver instance: " + getDriver().toString());
+
+            getDriver().manage().timeouts().pageLoadTimeout(GlobalConfig.pageLoadTimeoutInSec, TimeUnit.SECONDS);
+
             context.setAttribute("WebDriver", getDriver());
             context.setAttribute("logger", logger);
         }
     }
 
-    public WebDriver getDriver() {
-        return driver.get();
-    }
-
     @AfterMethod
     public void teardown() {
-        System.out.println(">>>>>>>>> after");
-        step("Cleaning up driver instance: " + driver.toString());
-        logger.info("Cleaning up driver instance: " + driver.toString());
+        step("Cleaning up driver instance: " + getDriver().toString());
         getDriver().quit();
+        step("Complete time: " + getDateTime("yyyy/MM/dd HH:mm:ss"));
     }
-//    public WebDriver getDriver() {
-//        return driver;
-//    }
-//
-//    public void setupBrowser(ITestContext context) {
-//        step("Running Test on '" + System.getProperty("os.name") + "' OS.");
-//        logger.info("Running Test on '" + System.getProperty("os.name") + "' OS.");
-//        step("Browser: " + browser.toUpperCase());
-//        logger.info("Browser: " + browser.toUpperCase());
-//
-//        switch (browser) {
-//            case "firefox":
-//                WebDriverManager.firefoxdriver().setup();
-//                driver = new FirefoxDriver();
-//                break;
-//            case "chromeHeadless":
-//                step("Browser: Chrome Headless");
-//                logger.info("Browser: Chrome Headless");
-//                WebDriverManager.chromedriver().setup();
-//                chromeOptions = new ChromeOptions();
-//                chromeOptions.addArguments("--no-sandbox");
-//                chromeOptions.addArguments("--disable-dev-shm-usage");
-//                chromeOptions.addArguments("--headless");
-//                driver = new ChromeDriver(chromeOptions);
-//                break;
-//            case "chrome":
-//                WebDriverManager.chromedriver().setup();
-//                chromeOptions = new ChromeOptions();
-//                chromeOptions.addArguments("--no-sandbox");
-//                chromeOptions.addArguments("--disable-dev-shm-usage");
-//                driver = new ChromeDriver(chromeOptions);
-//                break;
-//            case "edge":
-//                WebDriverManager.edgedriver().setup();
-//                driver = new EdgeDriver();
-//                break;
-//            case "ie":
-//                WebDriverManager.iedriver().setup();
-//                driver = new InternetExplorerDriver();
-//                break;
-//            case "opera":
-//                WebDriverManager.operadriver().setup();
-//                driver = new OperaDriver();
-//                break;
-//            case "safari":
-//                driver = new SafariDriver();
-//                break;
-//            default:
-//                step("No proper browser specified.");
-//                logger.info("No proper browser specified.");
-//        }
-//        if (driver != null) {
-//            step("Driver instance: " + driver.toString());
-//            logger.info("Driver instance: " + driver.toString());
-//            driver.manage().timeouts().pageLoadTimeout(48, TimeUnit.SECONDS);
-//            context.setAttribute("WebDriver", driver);
-//            context.setAttribute("logger", logger);
-//        }
-//    }
-//
-//    public void closeBrowser() throws Exception {
-//        step("Cleaning up driver instance: " + driver.toString());
-//        logger.info("Cleaning up driver instance: " + driver.toString());
-//        driver.quit();
-//    }
-//
-//    public WebDriver getChromeDriver() {
-//        WebDriverManager.chromedriver().setup();
-//        return new ChromeDriver();
-//    }
 
+    public String getDateTime(String format) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern(format);
+        LocalDateTime now = LocalDateTime.now();
+        return dtf.format(now);
+    }
 }
--- src/test/java/tests/Test.java
+++ src/test/java/tests/Test.java
@@ -1,14 +1,9 @@
 package test.java.tests;
 
-import java.util.Random;
-
 public class Test {
 
     public static void main(String[] args) {
-        Random random = new Random();
-        int rem = random.nextInt(10) % 2;
-        System.out.println(rem);
-        System.out.println(rem == 0);
+        System.out.println("-".repeat(4));
     }
 
 }
