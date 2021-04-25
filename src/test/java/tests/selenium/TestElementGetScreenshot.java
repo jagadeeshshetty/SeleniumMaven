4f68f86f67c7d9702a2a89415c249d3ecc556e5b
JagadeeshC <jagadeesh.cheluvaraj@gmail.com>
2021-04-25 10:21:25 +0530

    update

--- src/test/java/tests/selenium/TestElementGetScreenshot.java
+++ src/test/java/tests/selenium/TestElementGetScreenshot.java
@@ -1,2 +1,30 @@
-package test.java.tests.selenium;public class TestElementGetScreenshot {
+package test.java.tests.selenium;
+
+import io.qameta.allure.Description;
+import io.qameta.allure.Severity;
+import io.qameta.allure.SeverityLevel;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Listeners;
+import org.testng.annotations.Test;
+import test.java.helper.Base;
+import test.java.helper.Listener;
+import test.java.helper.Retry;
+
+import static io.qameta.allure.Allure.step;
+
+@Listeners({Listener.class})
+public class TestElementGetScreenshot extends Base {
+
+    @Test(enabled = true, retryAnalyzer = Retry.class)
+    @Severity(SeverityLevel.NORMAL)
+    @Description("Verify the element capture as an image using getScreenshotAs() method Selenium 4 feature.")
+    public void testElementGetScreenshot() {
+        step("tbd");
+        getDriver().get("http://the-internet.herokuapp.com/login");
+        WebElement submitButton = getDriver().findElement(By.xpath("//button[@type='submit']"));
+        System.out.println(submitButton.getScreenshotAs(OutputType.FILE));
+    }
+
 }
--- src/test/java/tests/selenium/TestSetup.java
+++ src/test/java/tests/selenium/TestSetup.java
@@ -1,2 +1,21 @@
-package test.java.tests.selenium;public class TestSetup {
+package test.java.tests.selenium;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.time.Duration;
+
+public class TestSetup {
+    public static void main(String[] args) throws InterruptedException {
+        WebDriverManager.chromedriver().setup();
+        WebDriver driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(8));
+
+        driver.get("http://www.google.com");
+        System.out.println(driver.toString());
+
+        Thread.sleep(4000);
+        driver.close();
+    }
 }
