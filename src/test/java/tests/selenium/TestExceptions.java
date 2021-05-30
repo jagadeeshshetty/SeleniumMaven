5b613ea9b8c8a60b9b8f38845f9d71cc35430677
JC <jagadeesh.cheluvaraj@gmail.com>
2021-05-30 08:28:30 +0530

    added test class

--- src/test/java/tests/selenium/TestExceptions.java
+++ src/test/java/tests/selenium/TestExceptions.java
@@ -1,35 +1,32 @@
-package test.java.tests.theInternet;
+package test.java.tests.selenium;
 
 import io.qameta.allure.Description;
 import io.qameta.allure.Severity;
 import io.qameta.allure.SeverityLevel;
-import org.testng.Assert;
 import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 import test.java.helper.Base;
 import test.java.helper.Listener;
 import test.java.helper.Retry;
-import test.java.pageobjects.theInternet.LoginPage;
 
 import static io.qameta.allure.Allure.step;
 
 @Listeners({Listener.class})
-public class TestLoginPage extends Base {
-
+public class TestExceptions extends Base {
     // Page object
-    private LoginPage loginPage;
+//    private LoginPage loginPage;
 
     @Test(priority = 1, enabled = true, retryAnalyzer = Retry.class)
     @Severity(SeverityLevel.BLOCKER)
     @Description("Verify the successful login with valid username and password.")
     public void testValidLogin() {
-        loginPage = new LoginPage(getDriver(), logger);
+//        loginPage = new LoginPage(getDriver(), logger);
 
         step("1. Login with valid username and password.");
-        loginPage.with("tomsmith", "SuperSecretPassword!");
+//        loginPage.with("tomsmith", "SuperSecretPassword!");
 
         step("2. Verify success message present");
-        Assert.assertTrue(loginPage.successMessagePresent(), "success message not present");
+//        Assert.assertTrue(loginPage.successMessagePresent(), "success message not present");
     }
-    
+
 }
