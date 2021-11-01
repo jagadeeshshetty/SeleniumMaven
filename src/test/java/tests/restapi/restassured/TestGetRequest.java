3e23edf180c0c56a661459f30ecd58b623a57cfa
JagadeeshC <jagadeesh.cheluvaraj@gmail.com>
2021-11-01 16:49:48 +0530

    restassured get request

--- src/test/java/tests/restapi/restassured/TestGetRequest.java
+++ src/test/java/tests/restapi/restassured/TestGetRequest.java
@@ -1,16 +1,84 @@
 package test.java.tests.restapi.restassured;
 
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.apache.http.HttpStatus;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 import java.net.URI;
 import java.net.URISyntaxException;
 
-import static io.restassured.RestAssured.when;
+import static io.restassured.RestAssured.given;
 
 public class TestGetRequest {
 
     @Test
-    void testGet() throws URISyntaxException {
-        when().get(new URI("http://192.168.0.126:8080/laptop-bag/webapi/api/ping/hi"));
+    void testPing() throws URISyntaxException {
+        Response response = given()
+                .when()
+                .get(new URI("http://192.168.0.126:8080/laptop-bag/webapi/api/ping/hi"));
+        System.out.println(response.asString());
     }
+
+    @Test
+    void testGetReqWithJSONRes() throws URISyntaxException {
+        Response response = given()
+                .accept(ContentType.JSON)
+                .when()
+                .get(new URI("http://192.168.0.126:8080/laptop-bag/webapi/api/all"));
+        System.out.println(response.asString());
+    }
+
+    @Test
+    void testGetReqWithXMLRes() throws URISyntaxException {
+        Response response = given()
+                .accept(ContentType.XML)
+                .when()
+                .get(new URI("http://192.168.0.126:8080/laptop-bag/webapi/api/all"));
+        System.out.println(response.asString());
+    }
+
+    @Test
+    void testGetReqWith200OKUsingThenReturn() throws URISyntaxException {
+        int returnStatusCode = given()
+                .when()
+                .get(new URI("http://192.168.0.126:8080/laptop-bag/webapi/api/all"))
+                .thenReturn()
+                .statusCode();
+        Assert.assertEquals(returnStatusCode, HttpStatus.SC_OK);
+    }
+
+    @Test
+    void testGetReqWith200OKUsingThen() throws URISyntaxException {
+        given()
+                .when()
+                .get(new URI("http://192.168.0.126:8080/laptop-bag/webapi/api/all"))
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    void testGetReqWithValidID() throws URISyntaxException {
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get(new URI("http://192.168.0.126:8080/laptop-bag/webapi/api/find/126"))
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_OK);
+    }
+
+    @Test
+    void testGetReqWithInValidID() throws URISyntaxException {
+        given()
+                .accept(ContentType.JSON)
+                .when()
+                .get(new URI("http://192.168.0.126:8080/laptop-bag/webapi/api/find/0"))
+                .then()
+                .assertThat()
+                .statusCode(HttpStatus.SC_NOT_FOUND);
+    }
+
 }
---
+++ src/test/java/tests/restapi/restassured/docs/README.md
@@ -0,0 +1,10 @@
+# RESTASSURED
+
+## GET Request
+
+- [Reference](https://javadoc.io/doc/io.rest-assured/rest-assured/latest/io/restassured/RestAssured.html#get-java.net.URI-)
+
+# Reference
+- [REST Assured](https://rest-assured.io/)
+- [REST Assured API Doc](https://javadoc.io/doc/io.rest-assured/rest-assured/latest/io/restassured/RestAssured.html)
+
