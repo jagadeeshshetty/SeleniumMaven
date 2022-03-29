c4ca41647b3f31de469a728208018cadf791e8fe
Jagadeesh C <jagadeesh.cheluvaraj@gmail.com>
2022-03-29 23:28:00 +0530

    Java8Features

--- src/test/java/tests/selenium/Java8Features.java
+++ src/test/java/tests/selenium/Java8Features.java
@@ -12,7 +12,9 @@ import test.java.helper.Listener;
 import test.java.helper.Retry;
 
 import java.io.IOException;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Listeners({Listener.class})
 public class Java8Features extends Base {
@@ -25,10 +27,43 @@ public class Java8Features extends Base {
         allureHelper.report("1. Access the test URL.");
 
         List<WebElement> archiveDates = getDriver().findElements(By.xpath("//*[@class='widget widget_archive']//a"));
-        allureHelper.report("2. List all the archive dates.");
 
+        // For Loop
+        allureHelper.report("2. List all the archive dates using 'for()' loop.");
         for (WebElement archiveDate : archiveDates) {
             allureHelper.report("....Archive Dates: " + archiveDate.getText(), archiveDate);
         }
+
+        // For Each Loop
+        allureHelper.report("3. List all the archive dates using 'forEach()' loop.");
+        archiveDates.forEach(date -> {
+            try {
+                allureHelper.report("....Archive Dates: " + date.getText(), date);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        });
     }
+
+    @Test(priority = 1, enabled = true, retryAnalyzer = Retry.class)
+    @Severity(SeverityLevel.MINOR)
+    @Description("Demo of Iterating Maps using 'for' loop.")
+    public void testMapsWithForAndForEachLoop() {
+        Map<String, Integer> map = new HashMap<>();
+        map.put("Chrome", 99);
+        map.put("Chrome", 98);
+        map.put("Firefox", 98);
+        map.put("Safari", 15);
+
+        // For loop
+        for (Map.Entry<String, Integer> entry : map.entrySet()) {
+            String k = entry.getKey();
+            Integer v = entry.getValue();
+            System.out.println("Key : " + k + ", Value : " + v);
+        }
+
+        // For Each loop
+        map.forEach((k, v) -> System.out.println("Key : " + k + ", Value : " + v));
+    }
+    
 }
